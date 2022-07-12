c44c2a02c178974fb37e5931cabcfadcf5fe2449
Brian Friel <briandfriel@gmail.com>
2022-07-11 20:19:46 -0700

    Complete tutorial

--- components/PingButton.tsx
+++ components/PingButton.tsx
@@ -1,16 +1,46 @@
-import { FC, useState } from 'react'
-import styles from '../styles/PingButton.module.css'
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import {
+  PublicKey,
+  Transaction,
+  TransactionInstruction,
+} from "@solana/web3.js";
+import { FC, useState } from "react";
+import styles from "../styles/PingButton.module.css";
 
 export const PingButton: FC = () => {
+  const { connection } = useConnection();
+  const { publicKey, sendTransaction } = useWallet();
 
-    const onClick = () => {
-        console.log('Ping!')
-    }
-    
-	return (
-		<div className={styles.buttonContainer} onClick={onClick}>
-			<button className={styles.button}>Ping!</button>
-		</div>
-	)
-}
+  const onClick = () => {
+    if (!connection || !publicKey) return;
 
+    const programId = new PublicKey(
+      "ChT1B39WKLS8qUrkLvFDXMhEJ4F1XZzwUNHUt4AU9aVa"
+    );
+    const programDataAccount = new PublicKey(
+      "Ah9K7dQ8EHaZqcAsgBW8w37yN2eAy3koFmUn4x3CJtod"
+    );
+    const transaction = new Transaction();
+    const instruction = new TransactionInstruction({
+      keys: [
+        {
+          pubkey: programDataAccount,
+          isWritable: true,
+          isSigner: false,
+        },
+      ],
+      programId,
+    });
+    transaction.add(instruction);
+
+    sendTransaction(transaction, connection).then((signature) =>
+      console.log(signature)
+    );
+  };
+
+  return (
+    <div className={styles.buttonContainer} onClick={onClick}>
+      <button className={styles.button}>Ping!</button>
+    </div>
+  );
+};
